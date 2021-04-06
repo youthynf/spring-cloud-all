c6ea0d1323e0113c3814f520c10202a41d2ebcdd
yannengfa <940436475@qq.com>
2021-04-06 08:15:55 +0800

    创建provider服务注册到eureka

---
+++ Eureka-Provider/src/main/java/com/study/eureka_provider/MainController.java
@@ -0,0 +1,12 @@
+package com.study.eureka_provider;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/getHi")
+    public String getHi() {
+        return "Hi";
+    }
+}
